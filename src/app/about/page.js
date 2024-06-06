fdf1aaf91c9cceff5e6d9f449b4dc8e4aa1a750e
Bektas Keldibayev <bektaskeld@gmail.com>
2024-06-06 07:37:03 +0500

    adds metadata for every page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,10 @@
 import Footer from "../components/footer";
 import Team from "../components/about";
 import Header from "../components/header";
-
+export const metadata = {
+  title: "About",
+  description: "About",
+};
 export default function About() {
   return (
     <div>
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,10 @@
 import Card from "../components/posts";
 import Footer from "../components/footer";
 import Header from "../components/header";
+export const metadata = {
+  title: "Blog",
+  description: "Blog",
+};
 export default function Blog() {
   return (
     <div>
